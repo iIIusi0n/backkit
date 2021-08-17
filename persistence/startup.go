756f1a61cb5985c5631aa513264f3bf18417375e
iIIusi0n <iIIusi0n@wyvern.pw>
2021-08-17 23:00:57 +0900

    Add HKLM version

--- persistence/startup.go
+++ persistence/startup.go
@@ -73,3 +73,69 @@ func addCurrentFileToStartupUsingHkcuRunOnce(name string) error {
 	}
 	return addExternalFileToStartupUsingHkcuRunOnce(name, currentPath)
 }
+
+func AddStartupUsingHklmRun(args ...string) error {
+	switch len(args) {
+	case 1:
+		return addCurrentFileToStartupUsingHklmRun(args[0])
+	case 2:
+		return addExternalFileToStartupUsingHklmRun(args[0], args[1])
+	default:
+		return errors.New("wrong args passed")
+	}
+}
+
+func addExternalFileToStartupUsingHklmRun(name, path string) error {
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, `Software\Microsoft\Windows\CurrentVersion\Run`, registry.QUERY_VALUE|registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	if err := key.SetStringValue(name, path); err != nil {
+		return err
+	}
+	if err := key.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func addCurrentFileToStartupUsingHklmRun(name string) error {
+	currentPath, err := utils.GetCurrentPath()
+	if err != nil {
+		return err
+	}
+	return addExternalFileToStartupUsingHklmRun(name, currentPath)
+}
+
+func AddStartupUsingHklmRunOnce(args ...string) error {
+	switch len(args) {
+	case 1:
+		return addCurrentFileToStartupUsingHklmRunOnce(args[0])
+	case 2:
+		return addExternalFileToStartupUsingHklmRunOnce(args[0], args[1])
+	default:
+		return errors.New("wrong args passed")
+	}
+}
+
+func addExternalFileToStartupUsingHklmRunOnce(name, path string) error {
+	key, err := registry.OpenKey(registry.LOCAL_MACHINE, `Software\Microsoft\Windows\CurrentVersion\RunOnce`, registry.QUERY_VALUE|registry.SET_VALUE)
+	if err != nil {
+		return err
+	}
+	if err := key.SetStringValue(name, path); err != nil {
+		return err
+	}
+	if err := key.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func addCurrentFileToStartupUsingHklmRunOnce(name string) error {
+	currentPath, err := utils.GetCurrentPath()
+	if err != nil {
+		return err
+	}
+	return addExternalFileToStartupUsingHklmRunOnce(name, currentPath)
+}
