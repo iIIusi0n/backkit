055db9a0c7c44c65ba597d53e2f2707cec7631ed
iIIusi0n <iIIusi0n@wyvern.pw>
2021-08-22 23:28:37 +0900

    Add todo in handler

--- handler/doc.go
+++ handler/doc.go
@@ -1,4 +1,6 @@
 // Package to make connection for C&C server.
 // Handler will parse received command from C&C server.
 // And will execute command.
+// TODO: Add handler that using normal HTTP.
+// TODO: Add handler that using Tor HTTP.
 package handler
