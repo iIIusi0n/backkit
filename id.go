595e1807fd96f96fc93808d2c1806a1c15e6c6f9
iIIusi0n <iIIusi0n@wyvern.pw>
2021-08-25 00:20:19 +0900

    Remove hostname from id generation
    for tech issue

--- id.go
+++ id.go
@@ -3,7 +3,6 @@ package backkit
 import (
 	"crypto/sha256"
 	"fmt"
-	"os"
 
 	"github.com/denisbrodbeck/machineid"
 )
@@ -17,10 +16,6 @@ func GenerateUniqueID() (string, error) {
 	if err != nil {
 		return "<error>", err
 	}
-	host, err := os.Hostname()
-	if err != nil {
-		return "<error>", err
-	}
-	hashHexString := ByteToHexString(sha256.Sum256([]byte(fmt.Sprintf("%s|%s|%s", publicIP, id, host))))
+	hashHexString := ByteToHexString(sha256.Sum256([]byte(fmt.Sprintf("%s|%s", publicIP, id))))
 	return hashHexString, nil
 }
